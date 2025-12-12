f6be47eac0781a7a5f32bed2a1f258386b648067
StephenCotterrell <stephen@cotterrell.dev>
2025-12-12 13:27:59 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("The GEMINI_API_KEY could not be loaded.")
+
+client = genai.Client(api_key=api_key)
+
+
+def main():
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
